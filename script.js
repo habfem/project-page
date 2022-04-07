6f052fed430ee61354b9f78ea994cd43d1b7b091
Habeeb Olufemi Mustapha M <72679550+habfem@users.noreply.github.com>
2022-04-07 02:52:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 const projectName = 'my portfolio';
+const projectName = 'my portfolio';
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('nav-list')[0]
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
